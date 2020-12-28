37cfe8d59c84a1a4f85f4ffbb18ad48a5317e641
Hyunwoo Moon <mhw9163@gmail.com>
2020-12-28 12:52:12 -0800

    working

--- app.js
+++ app.js
@@ -88,3 +88,72 @@ app.get('/sum', (req, res) => {
     const c = A + B;
     res.send(`The sum of ${a} and ${b} is ${c}`);
 })
+
+//q2.
+
+app.get('/cipher', (req, res) => {
+    const text= req.query.text;
+    const shift=req.query.shift;
+//validation : both values are required, shift must be a number
+    if(!text){
+        res.status(400).send('please give the text');
+    }
+    if(!shift){
+        res.status(400).send('please give the shift');
+    }
+
+    const numShift = parseFloat(shift);
+    if(Number.isNaN(numShift)){
+        return res.status(400).send('shift must be a number');
+    }
+    //all valid, perform the task
+    //make the text uppercase for convenience
+    //the question did not say what to do with punctuation marks
+    //and numbers so we will ignore them and only convert letters.
+    //also just the 26 letters of the alphabet in typical use in the us
+    //and uk today. to support an international audience we will have to
+    //do more
+    //create a loop over the characters, for each letter, convert
+    //using the shift
+
+    const base = 'A'.charCodeAt(0);
+
+    const cipher =  text
+                    .toUpperCase()
+                    //create an array of characters
+                    .split('')
+                    .map(char => { 
+                        //map each original char to a converted char
+                        const code = char.charCodeAt(0);//get the char code
+
+                //if it is not one of the 26 letters ignore it
+                if(code < base || code > (base+26)){
+                    return char;
+                }
+    //otherwise convert it
+    //get the distance from a
+    let diff = code - base; 
+    diff= diff + numShift;
+    diff= diff%26;
+
+    //in case shift takes the value past z, cycle back to the beginning
+    const shiftedChar = String.fromCharCode(base+diff);
+    return shiftedChar;
+                    })
+                    //construct a string from the array
+                .join('');
+                //return the response
+    res.status(200).send(cipher);
+});
+
+//q3
+
+app.get('/lotto', (req, res) =>{
+    const numbers = req.query.numbers;
+
+    if(!numbers){
+        return res.status(400).send('please give the  numbers!!');
+    }
+
+    
+})
